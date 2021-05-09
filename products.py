ef6c846a443f3106af3fad197cce0cef737a2e0d
Ivy Wang <ivywty@gmail.com>
2021-05-09 19:38:39 +0800

    add f.write funtion

--- products.py
+++ products.py
@@ -6,8 +6,11 @@ while True:
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
 print(products)
+
 for product in products:
 	print(product[0], '的價格是', product[1])
-with open('products,csv', 'w') as f:
+
+with open('products.csv', 'w') as f:
+	f.write('商品名稱,商品價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
